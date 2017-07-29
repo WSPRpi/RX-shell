89177def51d54f75e32d001674e9ec5cc3adc5ac
Michael Rawson <mr644@cam.ac.uk>
2017-07-29 19:04:57 +0000

    Inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Shell based decoder for WSPR
---
+++ wspr_rx.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##Assorted insanity by M0WUT to be a better WSPR decoder
+
+buffer_num="0"
+while true
+do
+	minute=$(date +"%M")
+	second=$(date +"%S")
+	if [ "$second" = "00" ]
+	then
+		echo $(date +"%H-%M-%S")"  Recording"
+		sudo arecord -f S16_LE -r 96000 --duration=118 -q -t wav -D hw:0,0 -c 2 /mnt/ramdisk/test_"$buffer_num".wav
+		record_pid=$!
+		wait $record_pid
+		echo $(date +"%H-%M-%S")"  Recording Complete into buffer ""$buffer_num"
+		~/wsprd /mnt/ramdisk/test_"$buffer_num".wav &
+		decode_pid=$!
+		if [ "$buffer_num" = "0" ]
+		then
+			buffer_num="1"
+		else
+			buffer_num="0"
+		fi
+		echo "Switched to buffer ""$buffer_num"
+	fi
+done
