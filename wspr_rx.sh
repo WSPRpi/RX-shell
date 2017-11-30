b025a91ba8af9cd46461c0b82de27d550f25a3d6
M0WUT <dan.m0wut@gmail.com>
2017-11-30 19:40:52 +0000

    Finished modifying file for single shot mode

--- wspr_rx.sh
+++ wspr_rx.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 ##Assorted insanity by M0WUT to make a better WSPR decoder
 
-buffer_num="0"
-old_buffer="1"
-first="0"
-
-
 if [[ "$#" != 3 ]]
 then
 	echo "Usage is of the format ./wspr_rx.sh callsign locator frequency(in MHz)"
@@ -14,15 +9,15 @@ then
 fi
 echo "WSPR Decoder by M0WUT: Callsign = $1, Locator = $2, Frequency = $3"
 
-	minute=$(date +"%-M")
-	second=$(date +"%-S")
-sudo arecord -f S16_LE -r 48000 --duration=114 -q -t wav -D hw:0,0 -c 2 /mnt/ramdisk/test_"$buffer_num".wav 
+start_time=$(date +"%y%m%d %H%M")
+echo "Beginning recording"
+sudo arecord -f S16_LE -r 48000 --duration=114 -q -t wav -D hw:0,0 -c 2 /mnt/ramdisk/raw.wav
 echo "Recording complete"
-sox /mnt/ramdisk/test_"$buffer_num".wav -c 1 -r 12000 output.wav
+sox /mnt/ramdisk/raw.wav -c 1 -r 12000 output.wav
 echo "Decimation complete"
-~/wspr_rx/wsprd -wf "$3" output.wav >/dev/null 2>&1
+~/wspr_rx/wsprd -wf "$3" test.wav >/dev/null 2>&1
 sed -i 's/tput   //g' ALL_WSPR.TXT
-sed -i "s/^....../$(date +%y%m%d) $report_time/g" ALL_WSPR.TXT
+sed -i "s/^....../$start_time/g" ALL_WSPR.TXT
 echo ""
 echo ""
 cat ALL_WSPR.TXT
@@ -31,5 +26,7 @@ echo ""
 #curl -F allmept=@ALL_WSPR.TXT -F call="$1" -F grid="$2" wsprnet.org/meptspots.php >/dev/null 2>&1
 echo "Upload complete"
 rm ALL_WSPR.TXT
+rm raw.wav
+rm output.wav
 
 
