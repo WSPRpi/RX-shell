551aedac823a0683ea44b1d6a769156ce5cb63b3
M0WUT <danielmcgraw@btinternet.com>
2018-01-03 12:16:30 +0000

    v1.0, all working with pipes and pretty output

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Requisites:
 * libfftw3-3: apt install libfftw3-dev
 * arecord: should be already installed, if not apt install alsa-utils
 * sox: apt install sox
-* wsprd: git clone [https://github.com/WSPRpi/WSPR-Decoder.git](https://github.com/WSPRpi/WSPR-Decoder.git), make, copy output executable to path
+* wsprd: git clone [https://github.com/WSPRpi/WSPR-Decoder.git](https://github.com/WSPRpi/WSPR-Decoder.git), make, copy output executable to path and rename to wsprd: cp k9an-wsprd /usr/bin/wsprd
 
 sample.TXT is example output.
 
 To run continually, test.py is provided, please change details to your own.
-Line 7 should read "subprocess.call(['wspr-rx', 'YOUR_CALLSIGN', 'YOUR_LOCATOR', 'FREQUENCY_IN_MHZ'])"
+
--- test.py
+++ test.py
@@ -1,18 +1,26 @@
-import time  
+import time
 import subprocess
 import threading
 import sys
 
+#####################
+#Configuration stuff#
+#####################
+callsign = 'm0wut'
+locator = 'io93fp'
+freq = '14.0956' #in MHz
+
 def start_wspr_rx():
-	subprocess.call(['./wspr-rx', 'm0wut', 'io93fp', '14.0956'])
+	subprocess.call(['./wspr-rx', callsign, locator, freq])
 
 
 if __name__ == "__main__":
-	print("WSPR Decoder, M0WUT")
+	print("WSPR Decoder Test Program, M0WUT and M0IKY")
+	print("Callsign: {}, Locator: {}, Frequency(MHz): {}\r\n".format(callsign,locator,freq))
 	while (1):
 		current_time = time.gmtime()
 		if(current_time.tm_min % 2 == 0 and current_time.tm_sec == 0):
-			print("Starting WSPR decode at {}:{}:{}".format(current_time.tm_hour, current_time.tm_min, current_time.tm_sec))
+			print("Starting WSPR decode at {:02d}:{:02d}\n".format(current_time.tm_hour, current_time.tm_min))
 			try:
 				t=threading.Thread(target = start_wspr_rx, args=())
 				t.start()
